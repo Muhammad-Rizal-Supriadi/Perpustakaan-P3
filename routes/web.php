34bf04ebf764554ddba0645669647ea937aa369b
Muhammad-Rizal-Supriadi <mrsupriadi2000@gmail.com>
2022-01-01 16:59:35 +0700

    homepage

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ use Illuminate\Support\Facades\Http;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function(){
+    return view('welcome');
+});
 
 Route::get('/admin', function () {
     return view('Admin.first');
